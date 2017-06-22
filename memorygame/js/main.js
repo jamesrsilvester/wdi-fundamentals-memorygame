3eb15e04f083514165e6abea967343bc6552dc38
James <jamesrsilvester@gmail.com>
2017-06-22 13:35:34 -0700

    change to game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,4 +1,26 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+	 rank: "queen",
+	 suit: "hearts",
+	 cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	 rank: "queen",
+	 suit: "diamonds",
+	 cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	 rank: "king",
+	 suit: "hearts",
+	 cardImage: "images/king-of-hearts.png"
+	},
+	{
+	 rank: "king",
+	 suit: "diamonds",
+	 cardImage: "images/king-of-diamonds.png"
+	}
+
+]
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -8,10 +30,12 @@ if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
 }
 
 var flipCard = function(cardID) {
-console.log('User flipped ' + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+console.log('User flipped ' + cards[cardID].rank);
+console.log('User flipped ' + cards[cardID].suit);
+console.log('User flipped ' + cards[cardID].cardImage);
+	cardsInPlay.push(cards[cardID].rank);
 	checkForMatch();
 }
-flipCard(0);
-flipCard(2);
 
+flipCard(0,2);
+flipCard(2,0);
